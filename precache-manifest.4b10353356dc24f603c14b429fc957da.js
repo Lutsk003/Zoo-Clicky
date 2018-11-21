cc8247864f81a66383b5370564e41d4ae582f504
Ilya <ilyalutskiy@gmail.com>
2018-11-20 18:26:33 -0600

    Updates

---
+++ precache-manifest.4b10353356dc24f603c14b429fc957da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87c2c6f13bf6dc21f423",
+    "url": "/zoo-clicky/static/js/runtime~main.87c2c6f1.js"
+  },
+  {
+    "revision": "74b6d7c0c9c4df70862b",
+    "url": "/zoo-clicky/static/js/main.74b6d7c0.chunk.js"
+  },
+  {
+    "revision": "8fdf2345b7a4e11badcc",
+    "url": "/zoo-clicky/static/js/1.8fdf2345.chunk.js"
+  },
+  {
+    "revision": "74b6d7c0c9c4df70862b",
+    "url": "/zoo-clicky/static/css/main.e421e2b4.chunk.css"
+  },
+  {
+    "revision": "3fea343a0a4350dd38903f47a5b4852f",
+    "url": "/zoo-clicky/index.html"
+  }
+];
